c0145dcf0001111f6ad4d6f19872dd34ee1f271e
Arjun Kollipara <arjun.kollipara22@gmail.com>
2024-11-25 19:20:17 +0530

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("button").addEventListener('click',()=>{ str = document.getElementById("name").value;
-    if(str.length>25)alert("Name cannot be more than 25 characters"); alert("You have successfully registered");
+    if(str.length>70)alert("Name cannot be more than 70 characters"); alert("You have successfully registered");
     })
